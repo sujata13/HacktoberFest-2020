fdfacdd9cf7bb7b27426181e88368cd2821d9d70
Shubham <Shubhamsingla807@gmail.com>
2020-10-02 16:40:20 +0530

    Print n even natural No

---
+++ C/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n;
+    printf("Enter the value of n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    printf("%d ",2*i);
+    return 0;
+}
